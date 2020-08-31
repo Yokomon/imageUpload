662b5e34ab6b34f2fbbe3cc21a93201c9d49e35e
yokomon <marowmars@gmail.com>
2020-08-31 19:01:36 +0100

    mongoose connection string

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 const express = require("express"),
-  app = express();
+  app = express(),
+  mongoose = require("mongoose");
+require("dotenv").config();
+MONGO_URI = process.env.MONGO_URI;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Assign Mongoose connection to the variable db
+let db = mongoose.connection;
+db.on("open", () => {
+  console.log("Connected to MongoDB using Mongoose");
+});
 
 app
   .use(express.urlencoded({ extended: false }))
